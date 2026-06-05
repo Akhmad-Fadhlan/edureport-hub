8bf2e057f393c7307569bec733ccc78d4721b187
Akhmad-Fadhlan <73178458+Akhmad-Fadhlan@users.noreply.github.com>
2026-06-05 09:27:29 +0700

    Update notes.tsx

--- src/routes/_authed/notes.tsx
+++ src/routes/_authed/notes.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import {
-  Dialog,
+  Dialog, 
   DialogContent,
   DialogHeader,
   DialogTitle,
